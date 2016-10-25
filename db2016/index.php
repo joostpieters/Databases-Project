694c50f81642158be95535dd4b6244ade26c6b42
Ruben <ruben.cartuyvels@gmail.com>
2016-10-25 21:16:09 +0200

    first commit

---
+++ db2016/index.php
@@ -0,0 +1,18 @@
+<?php
+	
+	// Dit is de titel die op de pagina en in de menubalk
+	// zal verschijnen.
+	$title = "The English Wikipedia database of writers and books";
+
+	// Voer de inhoud van "top.inc.php" uit. Deze verzorgt de
+	// algemene pagina lay-out en het menu.
+	require("template/top.tpl.php");
+?>
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";?>
+Group members:<br />
+Thomas Corthouts<br />
+Robbert Vanherck<br />
+Ruben Cartuyvels<br />
+
